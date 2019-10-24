c8aaa080dd1d094e7244d0effe3e6b1ff2fe86db
Simran Pannu <simpannu14@gmail.com>
2019-10-24 18:29:05 -0400

    final version of area of circle

---
+++ Area/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Area
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the radius:");
+            double radius = double.Parse(Console.ReadLine());
+            if (radius > 0)
+            {
+                double pi = Math.PI;
+                Console.WriteLine("Area of the Circle is:" + pi * radius * radius + " cm^2");
+            }
+            else
+            {
+                Console.WriteLine("Radius cannot be negative.");
+
+            }
+            
+        }
+    }
+}
